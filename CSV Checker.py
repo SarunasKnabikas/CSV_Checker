d60b052359f1201f46a4517b81e206d0393d0a1f
Sarunas <sarunas.knabikas@gmail.com>
2020-03-26 12:01:45 +0000

    Added dynamic column number function. To be reused in other checks.

--- CSV Checker.py
+++ CSV Checker.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def csv_Checker():
     with open('Check/test.csv', 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
@@ -8,9 +9,22 @@ def csv_Checker():
         i = 1
         for row in reader:
             print(row)
+            print()
 
-def expected_csv_headers():
+
+def expected_csv_headers_check(row):
     with open('Parameters/Headers.csv', 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        i = reader.next()
+        i = next(reader)
+
+        return i
+
+
+def expected_csv_column_number_check():
+    with open('Parameters/Headers.csv', 'r') as csvfile:
+        r = csv.reader(csvfile, delimiter=',')
+        headers = next(r)
+        i = 0
+        for column_count in headers:
+            i = i + 1
         return i
