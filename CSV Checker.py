5a1831468e9561e72ac6ee7e7fa79d3f8f327857
Sarunas Knabikas <sarunas.knabikas@gmail.com>
2020-03-28 12:51:25 +0000

    Added additional test files. Created general CSV loading function. Cleaned the code to make the most use of csv loading function.

--- CSV Checker.py
+++ CSV Checker.py
@@ -1,7 +1,8 @@
 import csv
+from os import listdir
 
 
-def csv_Checker():
+def csv_checker():
     with open('Check/test.csv', 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         observed_output = []
@@ -12,19 +13,39 @@ def csv_Checker():
             print()
 
 
-def expected_csv_headers_check(row):
-    with open('Parameters/Headers.csv', 'r') as csvfile:
+def get_headers_parameter():
+    param_file = load_csv_file("Headers.csv", True)
+    headers_list = next(param_file)
+    return headers_list
+
+
+def get_column_number_parameter():
+    param_file = load_csv_file("Headers.csv", True)
+    headers_list = next(param_file)
+    header_count = 0
+    for column_count in headers_list:
+        header_count = header_count + 1
+    return header_count
+
+
+def load_csv_file(filename, param=False):
+    if param == True:
+        folder = "Parameters/"
+    else:
+        folder = "Check/"
+
+    with open(folder + filename, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        i = next(reader)
 
-        return i
+        return reader
 
 
-def expected_csv_column_number_check():
-    with open('Parameters/Headers.csv', 'r') as csvfile:
-        r = csv.reader(csvfile, delimiter=',')
-        headers = next(r)
-        i = 0
-        for column_count in headers:
-            i = i + 1
-        return i
+def get_all_check_filenames():
+    filenames = listdir('Check/')
+    suffix = ".csv"
+    return [filename for filename in filenames if filename.endswith(suffix)]
+
+
+filenames = get_all_check_filenames()
+for name in filenames:
+    print(name)
